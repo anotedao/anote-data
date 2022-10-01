0675cdaebe3d450505ede8b8373057968d917b97
Pragmatic <cryptopragmatic@protonmail.com>
2022-10-01 02:50:13 +0200

    first sync

--- const.go
+++ const.go
@@ -4,4 +4,10 @@ const (
 	Port = 5003
 
 	MonitorTick = 10
+
+	AnoteNodeURL = "https://nodes.anote.digital"
+
+	MobileAddress = "3ANzidsKXn9a1s9FEbWA19hnMgV9zZ2RB9a"
+
+	TelegramAddress = "3ANmnLHt8mR9c36mdfQVpBtxUs8z1mMAHQW"
 )
--- monitor.go
+++ monitor.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"time"
+
+	"github.com/wavesplatform/gowaves/pkg/client"
+	"github.com/wavesplatform/gowaves/pkg/proto"
 )
 
 type Monitor struct {
 }
 
+func (m *Monitor) loadMiners() {
+	cl, err := client.NewClient(client.Options{BaseUrl: AnoteNodeURL, Client: &http.Client{}})
+	if err != nil {
+		log.Println(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	addr, err := proto.NewAddressFromString(MobileAddress)
+	if err != nil {
+		log.Println(err)
+	}
+
+	entries, _, err := cl.Addresses.AddressesData(ctx, addr)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, m := range entries {
+		miner := &Miner{}
+		db.FirstOrCreate(miner, &Miner{Address: m.GetKey()})
+		miner.MiningHeight = m.ToProtobuf().GetIntValue()
+		db.Save(miner)
+	}
+}
+
 func (m *Monitor) start() {
 	for {
-		log.Println("Tick")
+		m.loadMiners()
+
+		log.Println("Done update.")
 
 		time.Sleep(time.Second * MonitorTick)
 	}
